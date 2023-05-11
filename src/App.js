59eccd2c273c4f639238792008d50f743f7ff9c4
AnjAlia1 <anjalishukla.ec@gmail.com>
2023-05-11 23:25:44 +0530

    creating layout of newscard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-import data from './data.js';
+import datas from "./datas.js";
+import './App.css';
+import { useEffect, useState } from "react";
+
+
+function Newscontainer () {
+  const [isReadMore, setIsReadMore] = useState(true);
+  const toggleReadMore = (e) => {
+    setIsReadMore(!isReadMore);
+    console.log(e.target)
+  };
+  return (
+    datas.map((data) => {
+      return (
+        <div className="news-box" >
+          <h3>{data.author}</h3>
+          <div key={data.id}>
+            <p value={data.id}>{isReadMore ? data.content.slice(0, 60) : data.content}
+              <span key={data.id} value={data.id} onClick={(e)=>toggleReadMore(e)}>{isReadMore ? '..Readmore' : 'showless'}</span>
+            </p>
+          </div>
+          <span>{data.time}</span>
+          <div>
+            <img src={data.imageUrl} className="news-img"></img>
+          </div>
+        </div>
+      )
+    })
+  )
+}
 
 function App() {
   return (
-    <div className="news-dashboard">
-      console.log("data",data);
-    
-        
-   
-    </div>
+      <div >
+        <Newscontainer/>
+      </div>
   )
 }
 
