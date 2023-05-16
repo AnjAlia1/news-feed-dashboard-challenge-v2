2e2d19f12b76e1a14b90c01644e0a3659f4143a9
AnjAlia1 <anjalishukla.ec@gmail.com>
2023-05-17 02:05:42 +0530

    using fetch API to get data

--- src/App.js
+++ src/App.js
@@ -3,38 +3,13 @@ import './App.css';
 import data from "./data.js";
 import './App.css';
 import { useEffect, useState } from "react";
-
-
-function Newscontainer () {
-  const [isReadMore, setIsReadMore] = useState(true);
-  const toggleReadMore = (e) => {
-    setIsReadMore(!isReadMore);
-    console.log(e.target)
-  };
-  return (
-    data.map((data) => {
-      return (
-        <div className="news-box" >
-          <h3>{data.author}</h3>
-          <div key={data.id}>
-            <p value={data.id}>{isReadMore ? data.content.slice(0, 60) : data.content}
-              <span key={data.id} value={data.id} onClick={(e)=>toggleReadMore(e)}>{isReadMore ? '..Readmore' : 'showless'}</span>
-            </p>
-          </div>
-          <span>{data.time}</span>
-          <div>
-            <img src={data.imageUrl} className="news-img"></img>
-          </div>
-        </div>
-      )
-    })
-  )
-}
+import   { Post } from'./newsfeed';
 
 function App() {
   return (
-      <div >
-        <Newscontainer/>
+      <div className='content-container' >
+        <Post />
+    
       </div>
   )
 }
---
+++ src/newsfeed.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+export const Post = () => {
+    const [isReadMore, setisReadMore] = useState(true);
+    const [result, setResult] = useState({});
+    const toggleReadMore = () => {
+        setisReadMore(!isReadMore)
+    }
+
+    useEffect(() => {
+        fetch('https://inshorts.deta.dev/news?category=science')
+          .then(res => {
+            return res.json();
+          })
+          .then((data) => {
+            setResult(data);
+            console.log('data useEffect', data)
+            console.log('data', data.data)
+            console.log('data usffect',  result.data)
+          },
+          (err) => {
+            return console.error(err)
+          })
+      }, [])
+
+    return (
+        result.data.map((data) => {
+            return (
+                <div >
+                    <div className="news-box">
+                    <div><h6>{data.author}</h6>
+                            <p value={data.id}>{isReadMore ? data.content.slice(0, 120) : data.content}
+                                <span key={data.id} value={data.id} onClick={(e) => toggleReadMore(e)}>{isReadMore ? '..Readmore' : 'showless'}</span>
+                            </p>
+                            <span>{data.time}</span>
+                        </div>
+                        <img src={data.imageUrl} className="news-img"></img>
+                        
+                    </div>
+                </div>
+            )
+        })
+    )
+}
+
