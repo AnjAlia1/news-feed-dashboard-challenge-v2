278782fef384c577e4cd61e05a6d52fae6df9753
bharat-patodi <bharatpatodi@gmail.com>
2023-05-20 02:03:13 +0530

    Improve useEffect method

--- src/newsfeed.js
+++ src/newsfeed.js
@@ -1,71 +1,89 @@
 import { useEffect, useState } from "react";
 
 export const Post = () => {
-    const [isReadMore, setisReadMore] = useState(true);
-    const [result, setResult] = useState({
-      "author": "Sakshita Khosla",
-      "content": "Mumbai witnessed the 'Zero Shadow Day', which was also witnessed by people in Bengaluru and Hyderabad recently. Pictures and videos of the rare phenomenon, in which no shadows of vertical objects are formed at solar noon, were shared online. People did experiments with cylindrical objects to demonstrate the phenomenon.",
-      "date": "16 May 2023,Tuesday",
-      "id": "b86dc42d972d4803b397615ab291fc3e",
-      "imageUrl": "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/05_may/16_tue/img_1684210545273_382.jpg?",
-      "readMoreUrl": "https://www.hindustantimes.com/cities/mumbai-news/mumbai-witnesses-zero-shadow-day-after-hyderabad-bengaluru-101684159619826-amp.html?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts ",
-      "time": "09:51 am",
-      "title": "Mumbai witnesses 'Zero Shadow Day', pics surface",
-      "url": "https://www.inshorts.com/en/news/mumbai-witnesses-zero-shadow-day-pics-surface-1684210904335"
-    });
-    const toggleReadMore = () => {
-        setisReadMore(!isReadMore)
-    }
-  
-    // useEffect(() =>{
-    //   const getData = async () =>{
-    //    const apiResult = await fetch('https://inshorts.deta.dev/news?category=science')
-    //     .then(res => {
-    //       return res.json();
-    //     })
-    //     // .then(data=>{})
-    //     .then(information => {
-    //       return information;
-    //       setResult(information);
-    //       // console.log('data useEffect', data)
-    //       // console.log('data', data.data)
-    //       // console.log('data usffect',  result.data)
-    //     })
-    //   }
-    //     getData();
-    //       // (err) => {
-    //       //   return console.error(err)
-    //       // })
-    //       // )
-    //   }, [])
+  const [isReadMore, setisReadMore] = useState(true);
+  const [result, setResult] = useState({
+    category: "science",
+    data: [
+      {
+        author: "Pragya Swastik",
+        content:
+          "Traffic-related air pollution causes memory loss and cognitive decline and triggers neurological pathways associated with the onset of Alzheimer's, according to a study by researchers from the University of California, Irvine. \"The link between air pollution and Alzheimer's is concerning...Our findings are just one example of what particulate matter can do to brain function,\" a researcher said.",
+        date: "15 May 2023,Monday",
+        id: "63e10e5e05ec41858d373a6504d5e9ca",
+        imageUrl:
+          "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/05_may/15_mon/img_1684146153022_205.jpg?",
+        readMoreUrl:
+          "https://www.timesnownews.com/health/exposure-to-traffic-related-air-pollution-impacts-brain-functioning-shows-study-article-100237462/amp?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts ",
+        time: "04:49 pm",
+        title: "Traffic-related air pollution weakens brain function: Study",
+        url: "https://www.inshorts.com/en/news/trafficrelated-air-pollution-weakens-brain-function-study-1684149573314",
+      },
+      
+    ],
+    success: true,
+  });
 
-    // useEffect(() =>{
-    //   const getData = async () =>{
-    //    const apiResult = await fetch('https://inshorts.deta.dev/news?category=science');
-    // const finalData = await apiResult.json();
-    // setResult(finalData);
-    //     // })
-    //   }
-    //     getData();
-    //   }, [])
+  //   Add a useeffect to fetch the data from the api using fetch method, then pipe the data in .then() methods and finally update the state using setResult method.
 
-    return(
-      result.data.map((val) =>{
-            return( 
-            <div >
-              <div><h6>{val.author}</h6>
-              <img src={val.imageUrl} className="news-img"></img> 
-                    <div className="news-box">
-                            <p value={val.id}>{isReadMore ? val.content.slice(0, 120) : val.content}
-                                <span key={val.id} value={val.id} onClick={(e) => toggleReadMore(e)}>{isReadMore ? '..Readmore' : 'showless'}</span>
-                            </p>
-                            <span>{val.time}</span>
-                        </div>   
-                    </div>
-                </div>
-            )
-        })  
-    )
-    
-    }
+  //   useEffect(() => {
+  //     const getData = async () => {
+  //       const apiResult = await fetch(
+  //         "https://inshorts.deta.dev/news?category=science"
+  //       );
+  //       const finalData = await apiResult.json();
+  //       setResult(finalData);
+  //       // })
+  //     };
+  //     getData();
+  //   }, []);
 
+  // useEffect(() => {
+  //     fetch("https://inshortsapi.vercel.app/news?category=science")
+  //         .then((res) => res.json())
+  //         .then((data) => setResult(data));
+  // }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://inshorts.deta.dev/news?category=science"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        setResult(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return result.data.map((val) => {
+    return (
+      <div>
+        <div>
+          <h6>{val.author}</h6>
+          <img src={val.imageUrl} className="news-img"></img>
+          <div className="news-box">
+            <p value={val.id}>
+              {isReadMore ? val.content.slice(0, 120) : val.content}
+              <span
+                key={val.id}
+                value={val.id}
+                onClick={(e) => console.log("")}
+              >
+                {isReadMore ? "..Readmore" : "showless"}
+              </span>
+            </p>
+            <span>{val.time}</span>
+          </div>
+        </div>
+      </div>
+    );
+  });
+};
