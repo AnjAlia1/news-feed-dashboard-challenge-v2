b3ea577202422d7b7d29af91f44a3fb049edf6d3
AnjAlia1 <anjalishukla.ec@gmail.com>
2023-05-27 17:50:35 +0530

    improve code for search functionality

--- src/App.js
+++ src/App.js
@@ -7,12 +7,52 @@ import   { Post } from'./newsfeed';
 import {Search} from './searchfunctionality';
 // import searchfunctionality from './searchfunctionality';
 
+const Searchresult = ({results}) =>{
+  console.log( results)
+ return(
+  <div className="search-result" > {results.map((result)=>{
+   return(
+     <>
+    <p>{result.title}</p>
+    </>
+   )
+  })}
+  </div>
+)
+//   // <h1>hey</h1>
+// //   result.map((newstitle)=>{
+// //     return(
+// //       <h1>newstitle</h1>
+// //     )
+// // // // console.log("newstitle",newstitle.title);
+// //   })
+//  )
+{/* <div className="news-div">
+        <div >
+          <h6 className="author-name">{result.title}</h6>
+          <img src={result.imageUrl} className="news-img"></img>
+          <div className="news-box">
+            {/* <p value={result.id}>
+              {isReadMore ? val.content.slice(0, 120) : val.content} */}
+              {/* <a key={result.id} value={result.id} href={val.readMoreUrl} target="*" > */}
+                {/* {isReadMore ? "..Readmore" : "showless"} */}
+              {/* </a> */}
+            {/* </p> */}
+      //       <span>{result.time}</span>
+      //     </div>
+      //   </div>
+      // </div> */}
+//  ) 
+}
 function App() {
+  const [results, setResults] = useState([]);
   return (
       <div >
-        <Search/>
+        <Search setResults={setResults}/>
+        <Searchresult results={results}/>
         <div className="content-container">
         <Post/>
+        
         </div>
       </div>
   )
--- src/searchfunctionality.js
+++ src/searchfunctionality.js
@@ -1,42 +1,43 @@
 import { useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
 
-export const Search = ()=>{
+export const Search = ({setResults})=>{
   const [input, setInput] = useState('');
-  const [result, setResult] = useState([]);
-  const Apicall =(input)=>{
-    const apiData = fetch("https://inshorts.deta.dev/news?category=science").then((res)=>res.json()).then((json)=>{
-    const data = (json.data.filter(task => task.title.toLowerCase().includes(input)));
-    
-   setResult(data);
-     
-  })
-}
+
+  const Fetchdata =(input)=>{
+     fetch("https://inshorts.deta.dev/news?category=science")
+     .then((res)=>res.json()).
+     then((json)=>{
+    const results = json.data.filter((user => {
+     return input&& user.title.toLowerCase().includes(input)
+    }));
+    console.log("data",typeof results); 
+    setResults(results);
+  });   
+  };
+
   
+
   const HandleChange =(input)=>{
        setInput(input);
-       SearchResult(input)
-       console.log("data",data)
-        Apicall()
-        console.log("datFa",data)
-      console.log("INPUT",input);
-     console.log("result",result);
+      Fetchdata(input);  
        
+      //  SearchResult(input)
+      //  console.log("data",result);
      }
-   const SearchResult =()=>{
-    // setResult(input);
-
-      return(
-        <div>{input}</div>
-      )
-    }
+  //  const SearchResult =()=>{
+  //   // setResult(input);
+  //     return(
+  //       <div>{input}</div>
+  //     )
+  //   }
     
     // Callapi(input);
     return(
     <div className='search-input'>
         <FaSearch className="Search-Bar"></FaSearch>
       <input className="input-for-search" value={input} onChange={(e)=>HandleChange(e.target.value)}></input>
-      <SearchResult  ></SearchResult>
+      {/* <SearchResult ></SearchResult> */}
     </div>
     )
 
